4729332ca5565b95538a14fe50db334166517f76
arindam <dasarindam342@gmail.com>
2025-09-17 11:26:04 +0530

    Add Day11.java to find the largest element in an array

---
+++ Day11.java
@@ -0,0 +1,33 @@
+// Find the largest element in array..
+import java.util.Scanner;
+
+public class  Day11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size of array
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input elements
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Assume first element is largest
+        int largest = arr[0];
+
+        // Compare with other elements
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+
+        // Print result
+        System.out.println("The largest element is: " + largest);
+    }
+}
